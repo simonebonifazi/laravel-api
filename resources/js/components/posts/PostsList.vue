95d3bf91af9c20cce80ea6b0b1c5860bb4c040ea
simonebonifazi <simonbonifraz@gmail.com>
2022-10-06 01:12:25 +0200

    loader added

--- resources/js/components/posts/PostsList.vue
+++ resources/js/components/posts/PostsList.vue
@@ -1,34 +1,43 @@
 <template>
   <section id="posts-list">
     <h2> Posts</h2>
-    <div v-if="posts.length" class="d-flex flex-row flex-wrap">
-        <PostCard v-for="post in posts" :key="post.id" :post="post"/>  
+    <LoaderApp v-if="isLoading"/>
+    <div v-else>
+
+        <div v-if="posts.length" class="d-flex flex-row flex-wrap">
+            <PostCard v-for="post in posts" :key="post.id" :post="post"/>  
+        </div>
+        <h4 v-else> Nessun post ...</h4>
     </div>
-    <h4 v-else> Nessun post ...</h4>
   </section>
 </template>
 
 <script>
+import LoaderApp from '../LoaderApp';
 import PostCard from './PostCard';
 export default {
 name: 'PostsList',
 components: {
     PostCard,
+    LoaderApp,
 },
 data(){
     return {
         posts: [],
+        isLoading: false,
     };
 },
 methods: {
     fetchPosts(){
+        this.isLoading = true;
         axios.get('http://localhost:8000/api/posts')
         .then( res => {
             this.posts = res.data;
         }).catch((err) => {
+            //to fix error
             console.error(err);
         }).then(() => {
-            console.info('chiamata terminata')
+            this.isLoading = false;
         });
     },
 } ,
