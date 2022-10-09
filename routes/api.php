fe104d198b5dfc065b96ce8823f17935eb950c64
simonebonifazi <simonbonifraz@gmail.com>
2022-10-09 21:22:52 +0200

    grouped by namespace api routes

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', 'Api\PostController@index' );
-Route::get('/posts/{id}', 'Api\PostController@show' );
+Route::namespace('Api')->group(function(){
+
+    Route::get('/posts', 'PostController@index' );
+    Route::get('/posts/{id}', 'PostController@show' );
+});
