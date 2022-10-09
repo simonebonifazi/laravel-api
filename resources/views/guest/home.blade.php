f5d56deabb82c9032cc4a7cfc58eff5a7451cb47
simonebonifazi <simonbonifraz@gmail.com>
2022-10-09 21:50:10 +0200

    imported fontawesome

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -13,7 +13,10 @@
     <script defer src="{{ asset('js/front.js') }}"></script>
     <!-- import style -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
-
+    <!-- import FA -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
+        integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=='
+        crossorigin='anonymous' />
 
 </head>
 
