e175d20b09b9be37ed7c72fb8cd7907f61f569b6
simonebonifazi <simonbonifraz@gmail.com>
2022-10-10 04:27:20 +0200

    fixed detail edit

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -86,7 +86,7 @@ class CategoryController extends Controller
     public function edit(Category $category)
     {
         
-        return view('admin.categories.edit', compact( 'categories'));
+        return view('admin.categories.edit', compact( 'category'));
 
     }
 
